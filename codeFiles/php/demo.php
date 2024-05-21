68cfce0900e4ee58de28b4619a27179fbc18f489
nullno <weijundong@nullno.com>
2024-05-22 00:26:05 +0800

    created demo.php

---
+++ codeFiles/php/demo.php
@@ -0,0 +1,36 @@
+// PHP 常用的代码功能
+
+// 输出内容：
+echo "Hello, World!";
+
+// 变量定义与使用：
+$name = "John";
+echo $name;
+
+// 条件判断：
+if ($condition) {
+    // 执行代码
+} else {
+    // 执行其他代码
+}
+
+// 循环：
+for ($i = 0; $i < 10; $i++) {
+      // 执行代码
+}
+
+// 数组操作：
+$array = [1, 2, 3];
+foreach ($array as $value) {
+        // 执行代码
+}
+
+// 函数定义：
+function myFunction() {
+      // 执行代码
+}
+
+// 文件操作：
+$file = fopen("file.txt", "r");
+// 读取文件内容等操作
+fclose($file);
