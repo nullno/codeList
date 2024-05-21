84f4c9e369fd8545dc7957333dc99ff327744106
nullno <weijundong@nullno.com>
2024-05-22 00:16:27 +0800

    created demo.cpp

---
+++ codeFiles/C/demo.cpp
@@ -0,0 +1,37 @@
+//  C++常用的代码功能示例：
+
+// 1.输入输出：
+    std::cout << "Hello, World!" << std::endl;
+    int num;
+    std::cin >> num;
+
+// 2.条件判断：
+    if (condition) {
+      // 执行代码
+    } else {
+      // 执行其他代码
+    }
+
+// 3.循环：
+    for (int i = 0; i < n; i++) {
+      // 执行代码
+    }
+    while (condition) {
+      // 执行代码
+    }
+
+// 4.函数定义：
+    int add(int a, int b) {
+      return a + b;
+    }
+
+// 5.类和对象：
+    class MyClass {
+    public:
+      void myMethod() {
+        // 执行代码
+      }
+    };
+    MyClass obj;
+    obj.myMethod();
+
